9f221b9a2d184f36f6e2b4e24d09b38911d5ee30
RajKKapadia <raajforyou@gmail.com>
2025-11-03 19:27:32 +0530

    casting port to int

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
 
     import uvicorn
 
-    uvicorn.run(app="src.main:app", port=os.getenv("PORT", 8080), reload=True)
+    uvicorn.run(app="src.main:app", port=int(os.getenv("PORT", 8080)), reload=True)
